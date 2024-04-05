b56a072cf9bfd20c8194a6ba60558ad6b898f7e3
JavierBla <95310950+JavierBla@users.noreply.github.com>
2024-04-05 10:09:52 +0200

    Update Main.kt
    Agregado el grupo étnico al cálculo

--- TFG/src/main/kotlin/Main.kt
+++ TFG/src/main/kotlin/Main.kt
@@ -13,16 +13,20 @@ fun main() {
     print("Ingrese el valor de creatinina en suero: ")
     val creatininaSuero = scanner.nextDouble()
 
-    val resultadoTFG = calcularCKDEPI(edad, sexo, creatininaSuero)
+    print("Ingrese la etnia (Blanco | Negro))")
+    val grupoEtnico = scanner.next().lowercase(Locale.getDefault())
+
+    val resultadoTFG = calcularCKDEPI(edad, sexo, creatininaSuero, grupoEtnico)
     println("El Filtrado Glomerular Estimado (CKD-EPI) es: $resultadoTFG mL/min/1.73m²")
 }
 
-fun calcularCKDEPI(edad: Int, sexo: String, creatininaSuero: Double): Double {
+fun calcularCKDEPI(edad: Int, sexo: String, creatininaSuero: Double, grupoEtnico: String): Double {
     // Fórmula CKD-EPI para calcular el filtrado glomerular estimado
     val factorSexo = if (sexo == "hombre") 1.0 else 0.742
     val factorEdad = Math.pow(0.993, edad.toDouble())
+    val factorEtnia = if (grupoEtnico == "blanco") 1.0 else 1.159
 
-    val tfg = 141 * factorEdad * factorSexo * Math.min(creatininaSuero / 0.9, 1.0).pow(-0.411) * Math.max(creatininaSuero / 0.9, 1.0).pow(-1.209)
+    val tfg = 141 * factorEdad * factorSexo * Math.min(creatininaSuero / 0.9, 1.0).pow(-0.411) * Math.max(creatininaSuero / 0.9, 1.0).pow(-1.209) * factorEtnia
 
     return tfg
 }
